80b6e8088207be550c7a6a11688838c0d336308b
callmeknuts <callmeknuts@gmail.com>
2015-09-30 16:17:37 -0500

    created a diplodocus Dino and called the roar method

---
+++ DinoSupervisor.java
@@ -0,0 +1,27 @@
+package mike;
+
+public class DinoSupervisor {
+
+    public static void main(String[] args) {
+
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velciraptor = new Dinosaur();
+
+        Dinosaur diplodocus = new Dinosaur();
+
+        t_rex.color = "red";
+        t_rex.diet = "straight meat";
+        t_rex.name = " Percival";
+        t_rex.species = "Tyrannosaurus rex";
+
+        velciraptor.color = "green";
+        velciraptor.diet = "straight meat";
+        velciraptor.name = "Jerry";
+        velciraptor.species = "Velociraptor";
+
+        System.out.println("My name is " + t_rex.name + ", I'm a " + t_rex.species + ", my favorite color is " + t_rex.color + " and I eat " + t_rex.diet );
+        t_rex.roar();
+        velciraptor.roar();
+        diplodocus.roar();
+    }
+}
---
+++ Dinosaur.java
@@ -0,0 +1,16 @@
+package mike;
+
+/**
+ * Created by Mike on 9/30/2015.
+ */
+public class Dinosaur {
+
+    String color;
+    String name;
+    String diet;
+    String species;
+
+    public void roar (){
+        System.out.println("GRRRR!!!!");
+    }
+}
